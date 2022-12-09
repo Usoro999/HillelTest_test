808218a85d7a70f391cd233426a16fec80a466a2
usoro999 <usoro999@gmail.com>
2022-12-09 17:31:25 +0200

    changed - JavaPage

--- src/main/java/Config/DataProvider.java
+++ src/main/java/Config/DataProvider.java
@@ -9,9 +9,13 @@
         static {
             stringList.add("Moloko");
             stringList.add("Kefir");
+            stringList.add("Kolbasa");
+            stringList.add("voda");
 
             intList.add(71119900);
             intList.add(82239876);
+            intList.add(713119900);
+            intList.add(122395876);
         }
 
         public static String getRandomString() {
